1b54a81d9ce0254cd7bce4bc4f26f6f833c3eedf
andrews2 <andrewjshipman1@gmail.com>
2024-06-30 07:20:30 -0400

    set up basic articles service

--- services/news.js
+++ services/news.js
@@ -1,5 +1,20 @@
 module.exports = function(app) {
+    
+    const articles = [];
+
+    async function getArticles() {
+        if (articles.length === 0) {
+            try {
+                const data = await fetch(`http://api.mediastack.com/v1/news?access_key=${process.env.NEWS_API_KEY}&limit=100`);
+                const newArticles = await data.json();
+                articles = newArticles.data;
+            } catch(e) {
+                console.error(e);
+            }
+        }
+    }
+
     app.get('/api/news', (req, res) => {
-        res.send('Hello News');
+        res.json({ articles });
     })
 }
